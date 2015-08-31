da620b073a82c8777e452bb6f82cd38ca7075585
Andrew Azarov <andrew@azar-a.net>
2015-08-31 09:18:37 +0200

    debug

--- lg.py
+++ lg.py
@@ -172,6 +172,7 @@ class lookingglass(object):
         for line in read_data:
             if command in line or 'exit' in line:
                 indexes.append(read_data.index(line))
+        print indexes
         pre_return = read_data[min(indexes) + 1, max(indexes)]
         return str(os.linesep.join(pre_return)).strip()
 
